caeb190dd6737b750670b4688b326d4443540bec
zack_zheng <zack_zhengxiyun@163.com>
2022-07-18 21:57:25 +0800

    Leetcode:621

--- leetCodes/621/solution.js
+++ leetCodes/621/solution.js
@@ -1,18 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /**
  * @param {character[]} tasks
  * @param {number} n
  * @return {number}
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// 1. 如果冷却时间填满，则时间为任务数量
+// 2. 如果冷却时间无法填满，则时间为： 出现最多次数任务n次，m种任务出现n次，冷却时间k
+// 如果任务数量大于冷却时间，则一定会填满，应该取1，2最大值
+// (n-1)*(k+1)+m
+/**
+ *
+ * A [B] [C]
+ * A [B] []
+ * A [B] []
+ * A B
+ */
 const leastInterval = function (tasks, n) {
-  let cnt = {}
+  const cnt = '0'.repeat(26).split('').map(item => item * 1)
   for (let i = 0; i < tasks.length; i++) {
-    if (!cnt[tasks[i]])
-      cnt[tasks[i]] = 0
-    cnt[tasks[i]] += 1
+    const idx = tasks[i].charCodeAt() - 65
+    cnt[idx]++
   }
-  cnt = Object.values(cnt).sort()
+  cnt.sort((a, b) => a - b)
   let m = 0
-  for (let i = cnt.length - 1; i >= 0 && cnt[i] === cnt[cnt.length - 1]; i--, m++);
-  return Math.max(tasks.length, (cnt[cnt.length - 1] - 1) * (n - 1) + m)
+  for (let i = 25; i >= 0 && cnt[i] === cnt[25]; i--, m++);
+  return Math.max(tasks.length, (cnt[25] - 1) * (n + 1) + m)
 }
