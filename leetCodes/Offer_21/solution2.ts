699a6e9c098130948e9d9f13d934b4b049b21c0e
zack zheng <zack_zhengxiyun@163.com>
2022-12-19 15:11:41 +0800

    no message

---
+++ leetCodes/Offer_21/solution2.ts
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function exchange(nums: number[]): number[] {
+  const swap = (x, y) => {
+    const temp = nums[x]
+    nums[x] = nums[y]
+    nums[y] = temp
+  }
+  if (nums.length === 0)
+    return []
+  let x = 0
+  let y = nums.length - 1
+  do {
+    while (x < nums.length && nums[x] % 2) x++
+    while (y >= 0 && nums[y] % 2 === 0) y--
+    if (x <= y) {
+      swap(nums[x], nums[y])
+      x++
+      y--
+    }
+  } while (x <= y)
+}
