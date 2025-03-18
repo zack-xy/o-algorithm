768da473465e8a5791e0e6fa21ae2c5107e33be4
zack zheng <zack_zhengxiyun@163.com>
2025-03-18 10:57:05 +0800

    1

---
+++ dataStructure/链表/ListNodeSimple.java
@@ -0,0 +1,11 @@
+package dataStructure.链表;
+
+public class ListNodeSimple {
+    public int val;
+    public ListNodeSimple next;
+
+    ListNodeSimple(int x) {
+        val = x;
+        next = null;
+    }
+}
