0b79e7dfdaa49206f4e46ed31534b8a9731283c8
zack zheng <zack_zhengxiyun@163.com>
2025-04-24 16:59:20 +0800

    update

---
+++ algorithm/链表常见题/Leetcode/deleteNode.java
@@ -0,0 +1,20 @@
+package algorithm.链表常见题.Leetcode;
+
+import dataStructure.链表.ListNode;
+
+public class deleteNode {
+    /**
+     *
+     * [237. 删除链表中的节点](https://leetcode.cn/problems/delete-node-in-a-linked-list/?envType=problem-list-v2&envId=linked-list)
+     *
+     * 老实说，根本没读懂题目
+     * 我好像智商受到暴击
+     */
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
+
