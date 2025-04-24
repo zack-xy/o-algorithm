53e2f855cf22d9602d2d571b8de173f4a6e6c232
zack zheng <zack_zhengxiyun@163.com>
2025-04-24 13:59:44 +0800

    update

--- algorithm/链表常见题/环形链表问题/detectCycle.java
+++ algorithm/链表常见题/环形链表问题/detectCycle.java
@@ -3,6 +3,7 @@ package algorithm.链表常见题.环形链表问题;
 import dataStructure.链表.ListNode;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -23,6 +24,29 @@ public class detectCycle {
                 set.add(pos);
             }
             pos = pos.next;
+        }
+        return null;
+    }
+
+    // 解法二：双指针
+    public ListNode detectCycle2(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+            if (fast == slow) {
+                ListNode ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
         }
         return null;
     }
--- algorithm/链表常见题/环形链表问题/hasCycle.java
+++ algorithm/链表常见题/环形链表问题/hasCycle.java
@@ -24,4 +24,17 @@ public class hasCycle {
         }
         return false;
     }
+
+    // 解法二：快慢指针
+    public boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
 }
