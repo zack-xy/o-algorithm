ffc22e018f4c9d63b33e3b8a655dfa265afb737a
zack zheng <zack_zhengxiyun@163.com>
2025-05-06 17:50:10 +0800

    update

---
+++ algorithm/数组常见题/二分查找/search.java
@@ -0,0 +1,41 @@
+package algorithm.数组常见题.二分查找;
+
+public class search {
+    /**
+     *
+     * [704. 二分查找](https://leetcode.cn/problems/binary-search/description/)
+     *  最基础款的二分查找，有序数组，没有重复元素
+     */
+
+    // 二分查找写法一：区间是[left, right]
+    public static int search(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right) {
+            int mid=left+((right-left)>>1);
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target) right=mid-1;
+            if (nums[mid]<target) left=mid+1;
+        }
+        return -1;
+    }
+
+    // 二分查找写法二：区间是[left, right)
+    public static int search2(int[] nums, int target) {
+        int left=0;
+        int right=nums.length; // 不包含right，所以是nums.length
+        while(left < right) {
+            int mid=left+((right-left)>>1);
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target) right=mid;
+            if(nums[mid]<target) left=mid+1;
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        System.out.println(search(nums, 2));
+    }
+}
---
+++ algorithm/数组常见题/二分查找/searchRange.java
@@ -0,0 +1,48 @@
+package algorithm.数组常见题.二分查找;
+
+public class searchRange {
+    /**
+     *
+     * [34. 在排序数组中查找元素的第一个和最后一个位置](https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/)
+     * 排序数组，要求时间复杂度为O(log n)
+     */
+
+    // 解法一：循环，时间复杂度O(n)
+    public int[] searchRange(int[] nums, int target) {
+        int start = -1;
+        int end = -1;
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i] == target) {
+                if (start == -1) {
+                    start = i;
+                }
+                end = i;
+            }
+        }
+        return new int[]{start, end};
+    }
+
+    // 解法二：二分
+    public int[] searchRange2(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx =  binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1,-1};
+    }
+
+    private int binarySearch(int[] nums, int target, boolean lower) {
+        int left=0,right=nums.length-1,ans=nums.length;
+        while (left<=right) {
+            int mid = (left+right)>>1;
+            if (nums[mid]>target || (lower && nums[mid]>=target)) {
+                right=mid-1;
+                ans=mid;
+            } else {
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+}
