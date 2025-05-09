3a7b6fc142c0b7ee266decf3a625abe2e68ae4aa
zack zheng <zack_zhengxiyun@163.com>
2025-05-09 17:16:59 +0800

    update

---
+++ algorithm/栈常见题/括号问题/removeInvalidParentheses.java
@@ -0,0 +1,25 @@
+package algorithm.栈常见题.括号问题;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class removeInvalidParentheses {
+    /**
+     *
+     * [301. 删除无效的括号](https://leetcode.cn/problems/remove-invalid-parentheses/description/)
+     *
+     */
+
+    // 解法一：回溯 + 剪枝
+    // TODO 待研究
+    private List<String> res = new ArrayList<String>();
+    public List<String> removeInvalidParentheses(String s) {
+        return res;
+    }
+
+    // 解法二：广度优先搜索
+    // TODO 待研究
+
+    // 解法三：枚举状态子集
+    // TODO 待研究
+}
---
+++ algorithm/栈常见题/括号问题/scoreOfParentheses.java
@@ -0,0 +1,15 @@
+package algorithm.栈常见题.括号问题;
+
+public class scoreOfParentheses {
+    /**
+     *
+     * [856. 括号的分数](https://leetcode.cn/problems/score-of-parentheses/description/)
+     *
+     */
+
+    // TODO 待研究
+    public int scoreOfParentheses(String s) {
+        int n = s.length();
+        return n;
+    }
+}
