868e001ef4eb2ec9971971609ed5cf0a8fa867c7
zack zheng <zack_zhengxiyun@163.com>
2025-05-12 18:00:51 +0800

    update

---
+++ algorithm/哈希表常见题/n数之和/fourSumCount.java
@@ -0,0 +1,33 @@
+package algorithm.哈希表常见题.n数之和;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * [454. 四数相加 II](https://leetcode.cn/problems/4sum-ii/description/)
+ *
+ */
+public class fourSumCount {
+
+    // 分组 +
+    // TODO 待理解
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> countAB = new HashMap<Integer, Integer>();
+        for (int u : nums1) {
+            for (int v : nums2) {
+                countAB.put(u+v, countAB.getOrDefault(u+v, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (int u : nums3) {
+            for (int v : nums4) {
+                if (countAB.containsKey(-u-v)) {
+                    ans += countAB.get(-u-v);
+                }
+            }
+        }
+        return ans;
+    }
+
+}
