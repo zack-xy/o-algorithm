ceef5df9fd999887abb56302df249a77737d1678
zack zheng <zack_zhengxiyun@163.com>
2025-05-20 11:35:34 +0800

    update

---
+++ dataStructure/二分查找/binarysearch.java
@@ -0,0 +1,16 @@
+package dataStructure.二分查找;
+
+public class binarysearch {
+
+    // 二分查找递归写法
+    public int binarySearch(int[] array, int low, int high, int target) {
+        // 递归终止条件
+        if (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (array[mid] == target) return mid;
+            if (array[mid] > target) return binarySearch(array, low, mid-1, target);
+            if (array[mid] < target) return binarySearch(array, mid+1, high, target);
+        }
+        return -1;
+    }
+}
