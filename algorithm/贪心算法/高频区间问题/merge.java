87a5928ffb6d3a18926f108e6875bf29294cde2d
zack zheng <zack_zhengxiyun@163.com>
2025-05-30 15:21:26 +0800

    update

---
+++ algorithm/贪心算法/高频区间问题/merge.java
@@ -0,0 +1,31 @@
+package algorithm.贪心算法.高频区间问题;
+
+import java.util.Arrays;
+
+/**
+ *
+ * [56. 合并区间](https://leetcode.cn/problems/merge-intervals/description/)
+ *
+ */
+public class merge {
+
+    public int[][] merge(int[][] intervals) {
+        // 先按照区间起始位置排序
+        Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
+        // 遍历区间
+        int[][] res = new int[intervals.length][2];
+        int idx = -1;
+        for (int[] interval : intervals) {
+            // 如果结果数组是空的，或者当前区间的起始位置 > 结果数组中最后区间的终止位置，说明不重叠
+            // 则不合并，直接将当前区间加入结果数组
+            if (idx == -1 || interval[0] > res[idx][1]) {
+                res[++idx] = interval;
+            } else {
+                // 反之说明重叠，则将当前区间合并至结果数组的最后区间
+                res[idx][1] = Math.max(res[idx][1], interval[1]);
+            }
+        }
+        return Arrays.copyOf(res, idx + 1);
+    }
+
+}
